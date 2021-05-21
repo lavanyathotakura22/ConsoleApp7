71560bcc34a5d479df2e393fdd38404f71187ee7
lavanyathotakura22 <lavanyathotakura04@gmail.com>
2021-05-21 14:04:42 +0530

    methods added

---
+++ ConsoleApp7/Methods.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Person
+{
+    // Constructor that takes no arguments:
+    public Classes()
+    {
+        Name = "unknown";
+    }
+
+    // Constructor that takes one argument:
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; }
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+class TestPerson
+{
+    static void Main()
+    {
+        // Call the constructor that has no parameters.
+        var person1 = new Person();
+        Console.WriteLine(person1.Name);
+
+        // Call the constructor that has one parameter.
+        var person2 = new Person("Sarah Jones");
+        Console.WriteLine(person2.Name);
+        // Get the string representation of the person2 instance.
+        Console.WriteLine(person2);
+    }
+}
+// Output:
+// unknown
+// Sarah Jones
+// Sarah Jones
